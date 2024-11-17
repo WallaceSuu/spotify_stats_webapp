0de75d82630bb96131dbd2bbf2d2732e29532c4e
Wallace Su <wallacesu2015@gmail.com>
2024-11-17 18:39:59 -0500

    changed structure of front end
    moved the function that grabs the data from the backend to App.js rather than have it in Dashboard so it can be reused throughout other components. This makes the code more modular.

--- pythonProject/frontend/src/App.js
+++ pythonProject/frontend/src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
 import Dashboard from './components/Dashboard.js';
 import Header from './components/Header.js';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+      //fetching data from the backend
+      axios
+          .get("http://localhost:5000/api/data") //url of Flash endpoint with JSON data
+          .then((response) => {
+              console.log("data recieved: ", response.data);
+              setData(response.data);
+          })
+          .catch((err) => {
+              console.log("error fetching data: ", err.message)
+              setError(err.message);
+          });
+  }, [])
+
+  if (error) return <div>Error: {error}</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <div className="App">
       <header className="App-header">
         <Header></Header>
-        <Dashboard></Dashboard>
+        <Dashboard data = {data}></Dashboard>
       </header>
     </div>
   );
